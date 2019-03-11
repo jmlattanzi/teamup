c8f340ad60a6862c385c53c9b873552f53b331c7
jmlattanzi <jonathanlattanzi@gmail.com>
2019-03-11 09:56:05 -0500

    Added packages and routes

--- server/index.js
+++ server/index.js
@@ -2,4 +2,32 @@
 // logout
 // register
 
+require('dotenv').config()
 const express = require('express')
+const { json } = require('body-parser')
+const session = require('client-sessions')
+
+const app = express()
+app.use(session({
+    cookieName: 'user-session',
+    secret: process.env.SECRET
+}))
+
+app.get('/', (req, res) => {
+	res.json('working')
+})
+
+app.post('/login', (req, res) => {
+	res.json('login route')
+})
+
+app.post('/logout', (req, res) => {
+	res.json('logout')
+})
+
+app.post('/register', (req, res) => {
+	res.json('register')
+})
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => console.log(`Listening on localhost:${PORT}`))
